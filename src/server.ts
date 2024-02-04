393935f60b2e1f9fb6c752027e3164317d5a04e8
Alex Danilov <dai@vt.edu>
2024-02-04 03:06:33 -0600

    Implement send-tx and get-address

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,150 @@
 import express from "express";
 import cors from "cors";
 
-// simple express server
+import { getAccountNonce, createSmartAccountClient } from "permissionless";
+import {
+  UserOperation,
+  bundlerActions,
+  getSenderAddress,
+  getUserOperationHash,
+  waitForUserOperationReceipt,
+  GetUserOperationReceiptReturnType,
+  signUserOperationHashWithECDSA,
+} from "permissionless";
+import {
+  pimlicoBundlerActions,
+  pimlicoPaymasterActions,
+} from "permissionless/actions/pimlico";
+import {
+  Address,
+  Hash,
+  concat,
+  createClient,
+  createPublicClient,
+  encodeFunctionData,
+  http,
+  Hex,
+} from "viem";
+import {
+  generatePrivateKey,
+  privateKeyToAccount,
+  signMessage,
+} from "viem/accounts";
+import { avalancheFuji } from "viem/chains";
+import { createPimlicoPaymasterClient } from "permissionless/clients/pimlico";
+import { privateKeyToBiconomySmartAccount } from "permissionless/accounts";
+import { scrypt, toUtf8Bytes, id } from "ethers";
+
+const privateKeyFromSignature = async (signature: string): Promise<string> => {
+  const signatureBuffer = toUtf8Bytes(signature);
+  const salt = id("passkey");
+  return scrypt(signatureBuffer, salt, 1024, 8, 1, 16);
+};
 
 const PORT = 443;
+const API_KEY = "a1dc6007-223a-4485-b4d0-c0ee32a3ff62";
+const PAYMASTER_URL = `https://api.pimlico.io/v2/avalanche-fuji/rpc?apikey=${API_KEY}`;
+const BUNDLER_URL = `https://api.pimlico.io/v1/avalanche-fuji/rpc?apikey=${API_KEY}`;
+const ENTRY_POINT = "0x5ff137d4b0fdcd49dca30c7cf57e578a026d2789";
+
+const publicClient = createPublicClient({
+  transport: http("https://rpc.ankr.com/avalanche_fuji"),
+});
+const paymasterClient = createPimlicoPaymasterClient({
+  transport: http(PAYMASTER_URL),
+});
+
+// const account = await privateKeyToBiconomySmartAccount(publicClient, {
+//   privateKey: PRIVATE_KEY,
+//   entryPoint: "0x5ff137d4b0fdcd49dca30c7cf57e578a026d2789",
+// });
+
+// console.log(
+//   `Smart account address: https://43113.testnet.snowtrace.io/address/${account.address}`,
+// );
+
+// const smartAccountClient = createSmartAccountClient({
+//   account,
+//   chain: avalancheFuji,
+//   transport: http(BUNDLER_URL),
+//   sponsorUserOperation: paymasterClient.sponsorUserOperation,
+// })
+//   .extend(bundlerActions)
+//   .extend(pimlicoBundlerActions);
+
+// const gasPrices = await smartAccountClient.getUserOperationGasPrice();
+
+// console.log("Received gas prices:", gasPrices);
+
+// const txHash = await smartAccountClient.sendTransaction({
+//   to: "0xd8da6bf26964af9d7eed9e03e53415d37aa96045",
+//   value: 0n,
+//   data: "0x1234",
+//   maxFeePerGas: gasPrices.fast.maxFeePerGas,
+//   maxPriorityFeePerGas: gasPrices.fast.maxPriorityFeePerGas,
+// });
+
+// console.log(
+//   `User operation included: https://43113.testnet.snowtrace.io/tx/${txHash}`,
+// );
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+interface GetAddressRequest {
+  signature: string;
+}
+app.post("/get-address", async (req, res) => {
+  const { signature } = req.body as GetAddressRequest;
+  const privateKey = await privateKeyFromSignature(signature);
+  const account = await privateKeyToBiconomySmartAccount(publicClient, {
+    privateKey: privateKey as Hex,
+    entryPoint: ENTRY_POINT,
+  });
+  console.log(
+    `Smart account address: https://43113.testnet.snowtrace.io/address/${account.address}`,
+  );
+  res.json({ address: account.address });
+});
+
+interface SendTxRequest {
+  signature: string;
+  to: string;
+  value: number;
+  data: string;
+}
+app.post("/send-tx", async (req, res) => {
+  const { signature, to, value, data } = req.body as SendTxRequest;
+  const privateKey = await privateKeyFromSignature(signature);
+  const account = await privateKeyToBiconomySmartAccount(publicClient, {
+    privateKey: privateKey as Hex,
+    entryPoint: ENTRY_POINT,
+  });
+  console.log(
+    `Smart account address: https://43113.testnet.snowtrace.io/address/${account.address}`,
+  );
+  const smartAccountClient = createSmartAccountClient({
+    account,
+    chain: avalancheFuji,
+    transport: http(BUNDLER_URL),
+    sponsorUserOperation: paymasterClient.sponsorUserOperation,
+  })
+    .extend(bundlerActions)
+    .extend(pimlicoBundlerActions);
+  const gasPrices = await smartAccountClient.getUserOperationGasPrice();
+  console.log("Received gas prices:", gasPrices);
+  const txHash = await smartAccountClient.sendTransaction({
+    to: to as Address,
+    value: BigInt(value),
+    data: data as Hex,
+    maxFeePerGas: gasPrices.fast.maxFeePerGas,
+    maxPriorityFeePerGas: gasPrices.fast.maxPriorityFeePerGas,
+  });
+  console.log(
+    `User operation included: https://43113.testnet.snowtrace.io/tx/${txHash}`,
+  );
+  res.json({ txHash });
 });
 
 app.listen(PORT, () => {
