7d7dab23a76dce21f5d509fcfd2f74304722ccc8
Alex Danilov <dai@vt.edu>
2024-02-04 01:19:46 -0600

    Initial

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import fs from "fs";
+
+// simple express server
+
+const PORT = 443;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
